cd9fcdfde043623bd873b693e1526f78b0f42aaa
M-Abhilash-P <137744003+M-Abhilash-P@users.noreply.github.com>
2023-07-04 13:11:15 +0530

    Add files via upload

---
+++ Day 13/jqselector.js
@@ -0,0 +1,7 @@
+function func1(){
+    $("#div2").css("background-color"," #00539C");
+    $("#div1,#div3").css("background-color","#EEA47F");
+    $("#head").hide();
+    $("li:even").fadeToggle();
+
+}
